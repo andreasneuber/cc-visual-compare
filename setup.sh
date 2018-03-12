3377b2367170a71299d788618f2c843ac5a59c30
Andreas Neuber <github@neuber-web-solutions.de>
2018-03-12 23:50:11 +0200

    CC Visual Compare refactoring

--- setup.sh
+++ setup.sh
@@ -4,16 +4,8 @@
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get -y update
 sudo apt-get install -y php7.0 php7.0-common php7.0-gd php7.0-mysql php7.0-mcrypt php7.0-curl php7.0-intl php7.0-xsl php7.0-mbstring php7.0-zip php7.0-bcmath php7.0-iconv
+sudo apt-get install -y curl pinta
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
-sudo curl -LsS http://codeception.com/codecept.phar -o /usr/local/bin/codecept
-sudo chmod a+x /usr/local/bin/codecept
-
-sudo apt-get install -y imagemagick gnome-web-photo pinta
-
-
-## Setup - Codeception tests ##
-$ cd ~
-$ mkdir difference
-$ codecept bootstrap
-$ codecept generate:cest acceptance CreateScreenshotsCept
-$ codecept generate:cest acceptance CompareCept
+## Install Codeception ##
+composer require codeception/codeception --dev
